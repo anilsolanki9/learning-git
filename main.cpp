f8d6f40da2310b3e108e4fb68390696cc914de07
Anil Solanki <anilsolanki1192@gmail.com>
2025-09-09 13:40:11 +0530

    Update main.cpp corrected iostream and added a sum instruction

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <iostreamm>
+#include <iostream>
 using namespace std;
 
 int sum(int a, int b){
@@ -6,5 +6,6 @@ return a+b;
 }
 int main(){
   cout<<"Hello world"<<endl;
+  cout << "Sum of 3 and 4 is: "<< sum(3+4)<<endl;
   return 0;
 }
