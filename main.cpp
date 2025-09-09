376d3e92165a1029ba6b659bf2aa259f0362899e
anilsolanki9 <anilsolanki1192@gmail.com>
2025-09-09 15:40:20 +0530

    some new changed in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 using namespace std;
 
-int sum(int a, int b){
-return a+b;
+int sum(int a, int b)
+{
+    return a + b;
 }
-int main(){
-  cout<<"Hello world"<<endl;
-  cout << "Sum of 3 and 4 is: "<< sum(3+4)<<endl;
-  return 0;
+int main()
+{
+    cout << "Hello world" << endl;
+    cout << "Sum of 3 and 4 is: " << sum(3, 4) << endl;
+    return 0;
 }
